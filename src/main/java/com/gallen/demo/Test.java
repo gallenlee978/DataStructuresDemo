95bf45b70e624dd36940cada5c65e598e9984ab9
gallenlee978 <gallenlee978@163.com>
2018-10-29 16:30:02 +0800

    Hello World

---
+++ src/main/java/com/gallen/demo/Test.java
@@ -0,0 +1,14 @@
+package com.gallen.demo;
+
+/**
+ * @author gallen
+ * @description test
+ * @date 2018/10/29
+ * @time 16:28
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
