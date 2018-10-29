a55e22325c293ee90e4b3f2d8771c2a1a2a7862a
gallenlee978 <gallenlee978@163.com>
2018-10-29 16:28:52 +0800

    创建项目

---
+++ src/main/java/com/gallen/App.java
@@ -0,0 +1,13 @@
+package com.gallen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
